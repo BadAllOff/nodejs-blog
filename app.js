4bec708d39cd9a4745835956395ae4ca96bb5f26
Shamil <badalloffsh@gmail.com>
2022-05-14 16:41:42 +0200

    Add: ejs view renderer

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const fs = require('fs')
 const express = require('express');
-
 const app = express();
+
+// register view engine
+app.set('view engine', 'ejs');
+
 
 app.listen(3000);
 
 app.get('/', (reg, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index');
 });
 
 app.get('/about', (reg, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about');
 });
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404');
 });
