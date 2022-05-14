dda88b08b37ecabcffa385b5d42cc5732bd6a0dd
Shamil <badalloffsh@gmail.com>
2022-05-14 03:11:00 +0200

    404 redirect

---
+++ app.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
+app.get('/', (reg, res) => {
+    res.sendFile('./views/index.html', {root: __dirname});
+});
+
+app.get('/about', (reg, res) => {
+    res.sendFile('./views/about.html', {root: __dirname});
+});
+
+app.use((req, res) => {
+    res.sendFile('./views/404.html', {root: __dirname});
+});
