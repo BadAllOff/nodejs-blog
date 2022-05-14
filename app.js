ef65712a1b0a3cff8ca5f54adfce211783401797
Shamil <badalloffsh@gmail.com>
2022-05-14 19:00:59 +0200

    Add: public folder

--- app.js
+++ app.js
@@ -5,21 +5,22 @@ const app = express();
 // register view engine
 app.set('view engine', 'ejs');
 
-
 app.listen(3000);
 
+app.use(express.static('public'));
+
 app.get('/', (reg, res) => {
-    res.render('index', {title: 'random title'});
+    res.render('index', { title: 'random title' });
 });
 
 app.get('/about', (reg, res) => {
-    res.render('about', {title: 'random title'});
+    res.render('about', { title: 'random title' });
 });
 
 app.get('/blogs/create', (reg, res) => {
-    res.render('create', {title: 'random title'});
+    res.render('create', { title: 'random title' });
 });
 
 app.use((req, res) => {
-    res.status(404).render('404', {title: 'random title'});
+    res.status(404).render('404', { title: 'random title' });
 });
